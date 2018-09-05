39922f788085b776ce6acc45e48823d38a49cc84
Justus Bonz <jasdefer>
2018-09-05 12:26:53 +0200

    Added initial response objects and first unit tests

---
+++ ValidationHelper/UnitTests/ValidationHelperUnitTests/ValidationInfoTests/ConstructorTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ValidationHelper;
+
+namespace ValidationHelperUnitTests.ValidationInfoTests
+{
+    [TestClass]
+    public class ConstructorTests
+    {
+        [TestMethod]
+        public void CreateSuccess()
+        {
+            var info = new ValidationInfo();
+            Assert.AreEqual(true, info.IsValid, "The default constructor is wrongly invalid.");
+            Assert.IsNotNull(info.Errors, "The error list of the ValidationInfo is wrongly null.");
+            Assert.AreEqual(0, info.Errors.Count, $"The valid ValidationInfo contains {info.Errors.Count} error(s).");
+        }
+
+        [TestMethod]
+        public void StaticCreateSuccess()
+        {
+            var info = ValidationInfo.CreateSuccess;
+            Assert.AreEqual(true, info.IsValid, "The static method to create a success is wrongly invalid.");
+            Assert.IsNotNull(info.Errors, "The error list of the ValidationInfo is wrongly null.");
+            Assert.AreEqual(0, info.Errors.Count, $"The valid ValidationInfo contains {info.Errors.Count} error(s).");
+        }
+
+        [DataTestMethod]
+        [DataRow("error")]
+        [DataRow("Err0r5!!.,")]
+        public void CreateError(string error)
+        {
+            var info = new ValidationInfo(error);
+            Assert.AreEqual(false, info.IsValid, "The constructor to create a info with a single error is wrongly valid.");
+            Assert.IsNotNull(info.Errors, "The error list of the ValidationInfo is wrongly null.");
+            Assert.AreEqual(1, info.Errors.Count, $"The ValidationInfo created with a single error contains {info.Errors.Count} errors.");
+            Assert.AreEqual(error, info.Errors[0], $"The single error message is {info.Errors[0]} instead of {error}.");
+        }
+
+        [DataTestMethod]
+        [DataRow("error")]
+        [DataRow("Err0r5!!.,")]
+        public void StaticCreateError(string error)
+        {
+            var info = ValidationInfo.CreateFailure(error);
+            Assert.AreEqual(false, info.IsValid, "The constructor to create a info with a single error is wrongly valid.");
+            Assert.IsNotNull(info.Errors, "The error list of the ValidationInfo is wrongly null.");
+            Assert.AreEqual(1, info.Errors.Count, $"The ValidationInfo created with a single error contains {info.Errors.Count} errors.");
+            Assert.AreEqual(error, info.Errors[0], $"The single error message is {info.Errors[0]} instead of {error}.");
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(10)]
+        [DataRow(100)]
+        public void CreateErrors(int errorCount)
+        {
+            string[] errors = new string[errorCount];
+            for (int i = 0; i < errors.Length; i++)
+            {
+                errors[i] = $"error{i + 1}";
+            }
+
+            var info = new ValidationInfo(errors);
+            Assert.AreEqual(false, info.IsValid, "The constructor to create a info with a single error is wrongly valid.");
+            Assert.IsNotNull(info.Errors, "The error list of the ValidationInfo is wrongly null.");
+            Assert.AreEqual(errorCount, info.Errors.Count, $"The ValidationInfo created with a {errorCount} errors contains {info.Errors.Count} error(s).");
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(10)]
+        [DataRow(100)]
+        public void StaticCreateErrors(int errorCount)
+        {
+            string[] errors = new string[errorCount];
+            for (int i = 0; i < errors.Length; i++)
+            {
+                errors[i] = $"error{i + 1}";
+            }
+
+            var info = ValidationInfo.CreateFailure(errors);
+            Assert.AreEqual(false, info.IsValid, "The constructor to create a info with a single error is wrongly valid.");
+            Assert.IsNotNull(info.Errors, "The error list of the ValidationInfo is wrongly null.");
+            Assert.AreEqual(errorCount, info.Errors.Count, $"The ValidationInfo created with a {errorCount} errors contains {info.Errors.Count} error(s).");
+        }
+    }
+}
---
+++ ValidationHelper/ValidationHelper/ValidationInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace ValidationHelper
+{
+    /// <summary>
+    /// The ValidationInfo contains collection of errors. If no errors are provided the reponse is valid.
+    /// </summary>
+    public class ValidationInfo
+    {
+        /// <summary>
+        /// Create a new ValidationInfo. It indicates success until an error is added.
+        /// </summary>
+        public ValidationInfo() { }
+
+        /// <summary>
+        /// Create a new ValidationInfo. It indicates a failure with the provided error.
+        /// </summary>
+        /// <param name="error">The error with which the ValidationInfo is created.</param>
+        public ValidationInfo(string error)
+        {
+            Errors.Add(error);
+        }
+
+        /// <summary>
+        /// Create a new ValidationInfo. It indicates a failure with the provided errors.
+        /// </summary>
+        /// <param name="errors">The errors with which the ValidationInfo is created.</param>
+        public ValidationInfo(IEnumerable<string> errors)
+        {
+            Errors.AddRange(errors);
+        }
+
+        /// <summary>
+        /// The collection of all errors
+        /// </summary>
+        public List<string> Errors { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Is true, if there are no errors and false otherwise
+        /// </summary>
+        public virtual bool IsValid => Errors.Count == 0;
+
+        public static ValidationInfo CreateSuccess => new ValidationInfo();
+        public static ValidationInfo CreateFailure(string error) => new ValidationInfo(error);
+        public static ValidationInfo CreateFailure(IEnumerable<string> errors) => new ValidationInfo(errors);
+    }
+}

6a8a2ef965a00ff6c7c452e7898657abe2a72b7b
Justus Bonz <jasdefer>
2018-09-19 16:56:25 +0200

    Added success, info and warning messages
    They don't affect the is valid state of a validation info

---
+++ ValidationHelper/UnitTests/ValidationHelperUnitTests/ValidationInfoTests/WarningTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ValidationHelper;
+
+namespace ValidationHelperUnitTests.ValidationInfoTests
+{
+    [TestClass]
+    public class WarningTests
+    {
+        [TestMethod]
+        public void AddWarningToValid()
+        {
+            var info = ValidationInfo.CreateSuccess;
+            info.AddWarning("warning");
+            Assert.IsTrue(info.IsValid);
+            Assert.IsNotNull(info.Warnings);
+            Assert.AreEqual(1, info.Warnings.Count);
+            Assert.AreEqual("warning", info.Warnings[0]);
+        }
+
+        [TestMethod]
+        public void AddWarningToInvalid()
+        {
+            var info = ValidationInfo.CreateFailure("error");
+            info.AddWarning("warning");
+            Assert.IsFalse(info.IsValid);
+            Assert.IsNotNull(info.Warnings);
+            Assert.AreEqual(1, info.Warnings.Count);
+            Assert.AreEqual("warning", info.Warnings[0]);
+        }
+
+        [TestMethod]
+        public void AddMultipleWarningsToValid()
+        {
+            var info = ValidationInfo.CreateSuccess;
+            string[] warnings = new string[]
+            {
+                "warning1",
+                "warning2"
+            };
+            info.AddWarnings(warnings);
+            Assert.IsTrue(info.IsValid);
+            Assert.IsNotNull(info.Warnings);
+            Assert.AreEqual(2, info.Warnings.Count);
+            Assert.AreEqual("warning1", info.Warnings[0]);
+            Assert.AreEqual("warning2", info.Warnings[1]);
+        }
+
+        [TestMethod]
+        public void AddMultipleWarningsToInvalid()
+        {
+            var info = ValidationInfo.CreateFailure("error");
+            string[] warnings = new string[]
+            {
+                "warning1",
+                "warning2"
+            };
+            info.AddWarnings(warnings);
+            Assert.IsFalse(info.IsValid);
+            Assert.IsNotNull(info.Warnings);
+            Assert.AreEqual(2, info.Warnings.Count);
+            Assert.AreEqual("warning1", info.Warnings[0]);
+            Assert.AreEqual("warning2", info.Warnings[1]);
+        }
+    }
+}

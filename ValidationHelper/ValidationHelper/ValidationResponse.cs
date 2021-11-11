79a60f75000b3fae65a9a21069b6bc9d0a363469
Justus <justus.bonz@gmail.com>
2021-11-11 11:52:46 +0100

    Added global using and file scoped namespaces

--- ValidationHelper/ValidationHelper/ValidationResponse.cs
+++ ValidationHelper/ValidationHelper/ValidationResponse.cs
@@ -1,33 +1,32 @@
-﻿namespace ValidationHelper
+﻿namespace ValidationHelper;
+
+/// <summary>
+/// The ValidationResponse contains a generic property of the requested type and a collection of errors. If no errors are provided the response is valid, even if the generic property is assigned.
+/// </summary>
+/// <typeparam name="T">The type of the response.</typeparam>
+public class ValidationResponse<T> : ValidationInfo
 {
     /// <summary>
-    /// The ValidationResponse contains a generic property of the requested type and a collection of errors. If no errors are provided the response is valid, even if the generic property is assigned.
+    /// Create a new ValidaitonResponse.
+    /// It indicates success until an error is added.
+    /// The value of this ValidationResponse is not initialized.
     /// </summary>
-    /// <typeparam name="T">The type of the response.</typeparam>
-    public class ValidationResponse<T> : ValidationInfo
+    public ValidationResponse()
     {
-        /// <summary>
-        /// Create a new ValidaitonResponse.
-        /// It indicates success until an error is added.
-        /// The value of this ValidationResponse is not initialized.
-        /// </summary>
-        public ValidationResponse()
-        {
 
-        }
-
-        /// <summary>
-        /// Create a new ValidationResponse with the requested object. It indicates success until an error is added.
-        /// </summary>
-        /// <param name="value">The requested object.</param>
-        public ValidationResponse(T value)
-        {
-            Value = value;
-        }
-
-        /// <summary>
-        /// The requested value. The ValidationResponse can be invalid even if this value is set.
-        /// </summary>
-        public T Value { get; set; }
     }
+
+    /// <summary>
+    /// Create a new ValidationResponse with the requested object. It indicates success until an error is added.
+    /// </summary>
+    /// <param name="value">The requested object.</param>
+    public ValidationResponse(T value)
+    {
+        Value = value;
+    }
+
+    /// <summary>
+    /// The requested value. The ValidationResponse can be invalid even if this value is set.
+    /// </summary>
+    public T Value { get; set; }
 }
